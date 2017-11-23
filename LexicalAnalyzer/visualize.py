c1b1be0390cb7e14e73d2d1c05b33fcd6d499102
oddcoder <ahmedsoliman@oddcoder.com>
2017-11-24 00:10:50 +0200

    Fixing little bug in the visualization function

--- LexicalAnalyzer/visualize.py
+++ LexicalAnalyzer/visualize.py
@@ -5,13 +5,19 @@ def visualize(nfa, start, end, verbose=False):
     import xdot
     window = xdot.DotWindow()
     dotcode = 'digraph G {\n'
+    visited = set()
     for node in nfa.keys():
+        visited.add(node)
         if node == start:
             dotcode += str(node) + " [shape=box, style=filled, color=red];\n"
         if node in end:
             dotcode += str(node) + " [peripheries=2];\n"
         for node2,transition in nfa[node]:
             dotcode += str(node) + "->" + str(node2) + " [label=\"" + str(transition) + "\"];\n"
+    for node in end:
+        if node not in visited:
+            dotcode += str(node) + " [peripheries=2];\n"
+            visited.add(node)
     dotcode += '}'
     if verbose:
         print(dotcode)
