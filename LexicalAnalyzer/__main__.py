f4ef859df211c6dff87463589d65a7b0dc2f560d
mohamedmahmoud97 <mohamedmorsy8438@gmail.com>
2017-12-22 17:03:22 +0200

    Refactored some files in the project and tested it.

--- LexicalAnalyzer/__main__.py
+++ LexicalAnalyzer/__main__.py
@@ -1,18 +1,18 @@
 from DFA_Optimizer import *
 from nfa2dfa import *
-from visualize import *
 from regex2nfa import *
 from simulate import *
-import pdb
-pdb.set_trace()
-f = input("Grammer file Name: ")
+from visualize import *
+
+# pdb.set_trace()
+f = input("Grammar file name: ")
 grammer = open(f).read()
 nfa_start, nfa_end, nfa = regex2nfa(grammer)
 dfa_start, dfa_end, dfa = nfa2dfa(nfa, nfa_start, nfa_end)
 automagical_sort(dfa)
 #pdb.set_trace()
 dfa_start_min, dfa_end_min, dfa_min = dead_state_elimination(dfa, dfa_start, list(dfa_end))
-f = input("Code file name:")
+f = input("Code file name: ")
 code = open(f).read()
 token_list = simulate(dfa, dfa_start, dfa_end, code)
 for token, lexeme in token_list:
